f617a4f169960dd506c72a705d95813ca7365d2c
Max Beddies <ubyjy@student.kit.edu>
2018-07-25 14:27:58 +0200

    Fixed char pointer in Main.c

--- Main.c
+++ Main.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "algorithms/ShiftCipher.h"
 
 int main() {
 
-	char* message = "Hello, world!";
-	char* ciphertext = encrypt_sc_n(message, 13, 35);
-	char* plaintext = decrypt_sc_n(ciphertext, 13, 35);
+	char* message = strdup("Hello, world!");
+	char* ciphertext = encrypt_sc_all(message, 13, 35);
+	char* plaintext = decrypt_sc_all(ciphertext, 13, 35);
 
 	printf("Original message: %21s\n", message);
 	printf("Encrypted message: %20s\n", ciphertext);
 	printf("Decrypted message: %20s\n", plaintext);
 
+	encrypt_sc(message, 13, 20);
+	printf("Encrypted message: %20s\n", message);
+	decrypt_sc(message, 13, 20);
+	printf("Decrypted message: %20s\n", message);
+
+	free(message);
 	free(ciphertext);
 	free(plaintext);
 	return 0;
