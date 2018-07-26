4e664f9ba9b44343e3e1035d2523f0d1c7ceaa49
Max Beddies <ubyjy@student.kit.edu>
2018-07-26 02:35:40 +0200

    Started implementing selection functions

---
+++ algorithms/symmetric/DES/Selection.h
@@ -0,0 +1,22 @@
+#ifndef Selection_H
+#define Selection_H
+
+#include <stdint.h>
+
+uint8_t selection_1(uint8_t);
+
+uint8_t selection_2(uint8_t);
+
+uint8_t selection_3(uint8_t);
+
+uint8_t selection_4(uint8_t);
+
+uint8_t selection_5(uint8_t);
+
+uint8_t selection_6(uint8_t);
+
+uint8_t selection_7(uint8_t);
+
+uint8_t selection_8(uint8_t);
+
+#endif
